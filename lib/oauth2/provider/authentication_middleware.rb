57baf365702e35f830080c5a992d65753b7be6ba
Tom Ward <tom@popdog.net>
2010-11-23 23:27:22 +0000

    Split the rollercoaster if/else that made up the middleware into some simpler, shorter methods.
    Looking at the code, it almost seems as though the Mediator is now (or could now be) be merged into the handler.  Worth examining tomorrow I think.

--- lib/oauth2/provider/authentication_middleware.rb
+++ lib/oauth2/provider/authentication_middleware.rb
@@ -7,56 +7,90 @@ class OAuth2::Provider::AuthenticationMiddleware < Rack::Auth::AbstractHandler
   end
 
   def call(env)
-    @env = env
-    if request.token_from_header.nil? && request.token_from_param.nil?
-      oauth2
-      result = @app.call(env)
-      if oauth2.authentication_required?
-        request_oauth_authentication
+    Handler.new(@app, env).response
+  end
+
+  class Handler
+    attr_reader :app, :env, :request
+
+    delegate :access_token, :access_token=, :to => :mediator
+
+    def initialize(app, env)
+      @app = app
+      @env = env
+      @request = Request.new(env)
+      @env['oauth2'] = OAuth2::Provider::Mediator.new
+    end
+
+    def mediator
+      @env['oauth2']
+    end
+
+    def response
+      if request.has_token?
+        block_bad_request || block_invalid_tokens || handle_request
       else
-        result
+        result = app.call(env)
+        force_authentication || result
       end
-    elsif request.token_from_header && request.token_from_param
-      bad_request
-    elsif oauth2.access_token = OAuth2::Provider::AccessToken.find_by_access_token(request.token_from_header || request.token_from_param)
-      if oauth2.access_token.expired?
-        if oauth2.access_token.refreshable?
-          request_oauth_authentication 'expired_token'
-        else
-          request_oauth_authentication 'invalid_token'
-        end
+    end
+
+    def handle_request
+      result = app.call(env)
+      force_insufficient_scope || result
+    end
+
+    def block_bad_request
+      if request.token_from_param && request.token_from_header
+        bad_request
+      end
+    end
+
+    def block_invalid_tokens
+      self.access_token = OAuth2::Provider::AccessToken.find_by_access_token(request.token)
+      invalid_token(access_token) if access_token.nil? || access_token.expired?
+    end
+
+    def force_authentication
+      unauthorized if @env['oauth2'].authentication_required?
+    end
+
+    def force_insufficient_scope
+      forbidden if @env['oauth2'].insufficient_scope?
+    end
+
+    def invalid_token(token)
+      if token && token.expired? && token.refreshable?
+        unauthorized 'expired_token'
       else
-        result = @app.call(env)
-        if oauth2.insufficient_scope?
-          forbidden
-        else
-          result
-        end
+        unauthorized 'invalid_token'
       end
-    else
-      request_oauth_authentication 'invalid_token'
+    end
+
+    def forbidden
+      [403, {'Content-Type' => 'text/plain', 'Content-Length' => '0'}, []]
+    end
+
+    def bad_request
+      [400, {'Content-Type' => 'text/plain', 'Content-Length' => '0' }, []]
+    end
+
+    def unauthorized(error = nil)
+      challenge = "OAuth realm='Application'"
+      challenge << ", error='#{error}'" if error
+      [401, {'Content-Type' => 'text/plain', 'Content-Length' => '0', 'WWW-Authenticate' => challenge}, []]
     end
   end
 
-  def request_oauth_authentication(error = nil)
-    challenge = "OAuth realm='Application'"
-    challenge << ", error='#{error}'" if error
-    unauthorized challenge
-  end
-
-  def oauth2
-    @env['oauth2'] ||= OAuth2::Provider::Mediator.new
-  end
-
-  def request
-    request = Request.new(@env)
-  end
-
-  def forbidden
-    [403, {'Content-Type' => 'text/plain', 'Content-Length' => '0'}, []]
-  end
-
   class Request < Rack::Request
+    def token
+      token_from_param || token_from_header
+    end
+
+    def has_token?
+      !token.nil?
+    end
+
     def token_from_param
       params["oauth_token"]
     end
