1c144721b15bf1cd1542b9fbd9cc4f0440349484
Tom Ward <tom@popdog.net>
2011-06-21 11:54:14 +0100

    The example app should regrant existing authorizations where possible

--- examples/rails3-example/app/controllers/authorization_controller.rb
+++ examples/rails3-example/app/controllers/authorization_controller.rb
@@ -3,6 +3,7 @@ class AuthorizationController < ApplicationController
 
   before_filter :authenticate_account
   before_filter :block_invalid_authorization_code_requests
+  before_filter :regrant_existing_authorization
 
   def new
     @client = oauth2_authorization_request.client
@@ -14,5 +15,11 @@ class AuthorizationController < ApplicationController
     else
       deny_authorization_code
     end
+  end
+
+  private
+
+  def regrant_existing_authorization
+    oauth2_authorization_request.grant_existing! current_account
   end
 end
